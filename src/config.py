bd1f008dadd0b6d6b9651c8a1643ac220e38729d
Tri Nguyen <tringuyenminh23@gmail.com>
2024-01-18 16:34:56 +0200

    init llm service

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from dynaconf import Dynaconf
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+SRC_PATH = Path(__file__).parent.resolve()
+PROJECT_ROOT_PATH = SRC_PATH.parent
+MODEL_DIR_PATH = PROJECT_ROOT_PATH / "models"
 
 APPLICATION_NAME = "hate-speech-detection"
 
@@ -10,8 +16,18 @@ settings = Dynaconf(
 )
 
 
+class LLMConfig(BaseModel):
+    model_config = ConfigDict(protected_namespaces=())
+
+    model_name: str
+    model_configs: dict
+    prompt_configs: dict
+
+
 class RootConfig(BaseModel):
     log_level: str
+
+    llm: LLMConfig
 
 
 CONFIGS = RootConfig.model_validate({k.lower(): v for k, v in settings.as_dict().items()})
---
+++ src/llm.py
@@ -0,0 +1,68 @@
+from typing import Any, TypeVar
+
+from llama_cpp import Llama
+from llama_cpp.llama_grammar import LlamaGrammar, SchemaConverter
+from pydantic import BaseModel
+
+from .config import CONFIGS, MODEL_DIR_PATH
+from .logging import get_logger
+from .prompts import PromptTemplate
+
+LOGGER = get_logger("LLM")
+
+PydanticModel = TypeVar("PydanticModel", bound=BaseModel)
+
+
+class LLMError(Exception):
+    pass
+
+
+class LLMService:
+    def __init__(self) -> None:
+        cfg = CONFIGS.llm
+        try:
+            self.model = Llama(model_path=str(MODEL_DIR_PATH / cfg.model_name), **cfg.model_configs)
+        except Exception as e:
+            LOGGER.exception("Failed to initiate model %s with configs %s: %s", cfg.model_name, cfg.model_configs, e)
+        self.default_prompt_configs = cfg.prompt_configs
+
+    def run(
+        self,
+        prompt: PromptTemplate,
+        prompt_inputs: dict,
+        **kwargs: Any,
+    ) -> str:
+        formatted_prompt = prompt.format_inputs(inputs=prompt_inputs)
+        completion_kwargs = {**self.default_prompt_configs, **kwargs, "prompt": formatted_prompt}
+        try:
+            llm_out = self.model.create_completion(**completion_kwargs)
+        except Exception as e:
+            LOGGER.exception("Failed to get llm outputs with %s: %s", completion_kwargs, e)
+            raise LLMError() from e
+        try:
+            result = llm_out["choices"][0]["text"]
+        except Exception as e:
+            LOGGER.exception("Failed to parse llm output %s: %s", llm_out, e)
+            raise LLMError() from e
+        return result  # type: ignore
+
+    def run_parse_model(
+        self, prompt: PromptTemplate, prompt_inputs: dict, output_model: type[PydanticModel], **kwargs: Any
+    ) -> PydanticModel:
+        grammar = pydantic_model_to_llama_grammar(model=output_model)
+        llm_out = self.run(prompt=prompt, prompt_inputs=prompt_inputs, grammar=grammar, **kwargs)
+        try:
+            return output_model.model_validate_json(llm_out)
+        except Exception as e:
+            LOGGER.exception("Failed to parse LLM output to %s: %s", output_model.__name__, e)
+            raise LLMError() from e
+
+
+def pydantic_model_to_llama_grammar(model: type[BaseModel]) -> LlamaGrammar:
+    try:
+        converter = SchemaConverter({})
+        converter.visit(model.model_json_schema(), "")
+        return LlamaGrammar.from_string(grammar=converter.format_grammar(), verbose=False)
+    except Exception as e:
+        LOGGER.exception("Failed to generate LlamaGrammar from %s: %s", model.__name__, e)
+        raise LLMError() from e
