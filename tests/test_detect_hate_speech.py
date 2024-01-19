b84e40fb1cbc110233f99eb00b65d36192a77348
Tri Nguyen <tringuyenminh23@gmail.com>
2024-01-19 13:29:39 +0200

    add detect hatespeech evaluation test

--- tests/test_detect_hate_speech.py
+++ tests/test_detect_hate_speech.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 import pytest
+from sklearn import metrics
 
 from src.llm import LLMService, detect_hate_speech
+from src.logging import logger
+from tests.data.hatexplain import load_hatexplain_detection_cases
 
 
 @pytest.mark.parametrize(
@@ -17,6 +22,40 @@ def test_detect_hate_speech_simple(llm_service: LLMService, text: str, is_hate_s
     res = detect_hate_speech(llm=llm_service, text=text)
     assert res.is_hate_speech == is_hate_speech, f"Expected is_hate_speech {is_hate_speech}. Got {res}"
     if is_hate_speech:
-        assert res.target_group, f"Expected identify target_group. Got {res.target_group}"
+        assert res.target_of_hate, f"Expected identify target. Got {res.target_of_hate}"
     else:
-        assert not res.target_group, f"Expected no target group. Got {res.target_group}"
+        assert not res.target_of_hate, f"Expected no target. Got {res.target_of_hate}"
+
+
+@pytest.mark.evaluation
+def test_evaluate_detect_hate_speech_hatexplain(llm_service: LLMService, test_case_out_file: Path) -> None:
+    y_true, y_pred = [], []
+
+    with test_case_out_file.open(mode="w") as f:
+        for case in load_hatexplain_detection_cases():
+            f.write("Test case:\n")
+            f.write(case.model_dump_json(indent=2) + "\n")
+            y_true.append(case.is_hate_speech)
+            try:
+                res = detect_hate_speech(llm=llm_service, text=case.text)
+            except Exception as e:
+                logger.exception(e)
+                y_pred.append(-1)
+                continue
+            y_pred.append(res.is_hate_speech)
+            f.write("Result:\n")
+            f.write(res.model_dump_json(indent=2) + "\n")
+            f.write("-" * 10 + "\n")
+
+        precision, recall, f_score, support = metrics.precision_recall_fscore_support(
+            y_true=y_true, y_pred=y_pred, labels=[True, False]
+        )
+        for idx, case_name in enumerate(["Hate cases", "Not Hate cases"]):
+            f.write(
+                f"""{case_name}:
+- No. case: {support[idx]}
+- Precision: {precision[idx]}
+- Recall: {recall[idx]}
+- F-score: {f_score[idx]}
+"""
+            )
