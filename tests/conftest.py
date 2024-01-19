8718ad1439add32bf4b9b7a09c38acb843255deb
Tri Nguyen <tringuyenminh23@gmail.com>
2024-01-19 22:25:22 +0200

    add db migrations test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,11 @@ import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from httpx import Client
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from src.app import app, get_llm_service
+from src.config import CONFIGS
 from src.llm import LLMService
 from tests import TEST_DIR_PATH, TEST_OUTPUTS_DIR_PATH
 
@@ -14,11 +17,13 @@ from tests import TEST_DIR_PATH, TEST_OUTPUTS_DIR_PATH
 class _FLAGS:
     run_evaluation_tests = "--run-eval"
     run_comparison = "--output-diff"
+    run_db_integration_tests = "--run-db-integration"
 
 
 def pytest_addoption(parser) -> None:  # type: ignore
     parser.addoption(_FLAGS.run_evaluation_tests, action="store_true", help="run evaluation tests")
     parser.addoption(_FLAGS.run_comparison, action="store_true", help="show changes of test output file")
+    parser.addoption(_FLAGS.run_db_integration_tests, action="store_true", help="run DB integration tests")
 
 
 def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: list[pytest.Item]) -> None:
@@ -29,6 +34,12 @@ def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config
     for item in items:
         if "evaluation" in item.keywords and not run_eval_tests:
             item.add_marker(skip_eval_tests)
+
+    run_db_integration_tests = config.getoption(_FLAGS.run_db_integration_tests)
+    skip_db_integration_tests = pytest.mark.skip(reason=f"require {_FLAGS.run_db_integration_tests} to run")
+    for item in items:
+        if "alembic" in item.keywords and not run_db_integration_tests:
+            item.add_marker(skip_db_integration_tests)
 
 
 @pytest.fixture(scope="session")
@@ -69,3 +80,15 @@ def application(llm_service: LLMService) -> FastAPI:
 @pytest.fixture(scope="session")
 def client(application: FastAPI) -> Client:
     return TestClient(app=application)
+
+
+@pytest.fixture
+def alembic_engine() -> AsyncEngine:
+    return create_async_engine(url=CONFIGS.db.async_conn_str, future=True, poolclass=pool.NullPool)
+
+
+def _get_required_env_var(env_var: str) -> str:
+    val = os.getenv(env_var)
+    if not val:
+        raise KeyError(f"Missing required env var `{env_var}`")
+    return val
